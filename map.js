1007650d109386465c9b5dff2e33b7b11f1f5af9
ravenclaw1999 <702schowdhury@gmail.com>
2018-08-16 13:37:58 -0400

    map is working now

--- map.js
+++ map.js
@@ -8,116 +8,20 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 	}).addTo(mymap);
 
 var india_marker = L.marker([20.5937, 78.9629]).addTo(mymap);
-india_marker.bindPopup("Women suffer from domestic violence.");
+india_marker.bindPopup("<p>Women suffer from domestic violence.</p><p>Men are allowed to divorce their wives just by reciting 'I divorce you' three times.</p><p>Women and girls with disabilities struggle to seek justice for violence against them.");
 
+var pakistan_marker = L.marker([30.3753,69.3451]).addTo(mymap);
+pakistan_marker.bindPopup("<p>Men are still allowed to beat their wives.</p><p>21% of girls get married before the age of 18.</p><p>Violence against women in Pakistan includes rape, murder, acid attacks, and domestic violence.</p><p>Women are also victims of honor killing, which involves family members killing each other and not receiving any consequences.");
 
-    // //Marker code for Pakistan
-    // var pos_Pakistan = ol.proj.fromLonLat([ 69.3451,30.3753]);
-    // var markerPakistan = new ol.Overlay({
-    //      position: pos_Pakistan,
-    //      positioning: 'center-center',
-    //      element: document.getElementById('marker-Pakistan'),
-    //      stopEvent: false
-    //    });
-    //    map.addOverlay(markerPakistan);
-    //
-    //    var pakistan = new ol.Overlay({
-    //       position: pos_Pakistan,
-    //       element: document.getElementById('pakistan')
-    //     });
-    //     map.addOverlay(pakistan);
-    //
-    //     // Popup showing the position the user clicked
-    //     var popup_Pakistan= new ol.Overlay({
-    //       element: document.getElementById('popup-Pakistan')
-    //     });
-    //     map.addOverlay(popup_Pakistan);
-    //     //end marker
-    //
-    //   // Marker code for Afghanistan
-    //   var pos_Afghanistan = ol.proj.fromLonLat([ 67.7100,33.9391]);
-    //   var markerAfghanistan= new ol.Overlay({
-    //        position: pos_Afghanistan,
-    //        positioning: 'center-center',
-    //        element: document.getElementById('marker-Afghanistan'),
-    //        stopEvent: false
-    //      });
-    //      map.addOverlay(markerAfghanistan);
-    //
-    //      var Afghanistan = new ol.Overlay({
-    //         position: pos_Afghanistan,
-    //         element: document.getElementById('afghanistan')
-    //       });
-    //       map.addOverlay(Afghanistan);
-    //
-    //       // Popup showing the position the user clicked
-    //       var popup_afghanistan= new ol.Overlay({
-    //         element: document.getElementById('popup-Afghanistan')
-    //       });
-    //       map.addOverlay(popup_afghanistan);
-    //       //end marker
-    //
-    //     //Marker code for Egypt
-    //     var pos_Egypt = ol.proj.fromLonLat([ 30.8025, 26.8206]);
-    //     var markerEgypt= new ol.Overlay({
-    //          position: pos_Egypt,
-    //          positioning: 'center-center',
-    //          element: document.getElementById('marker-Egypt'),
-    //          stopEvent: false
-    //        });
-    //        map.addOverlay(markerEgypt);
-    //
-    //        var Egypt = new ol.Overlay({
-    //           position: pos_Egypt,
-    //           element: document.getElementById('egypt')
-    //         });
-    //         map.addOverlay(Egypt);
-    //
-    //         // Popup showing the position the user clicked
-    //         var popup_egypt= new ol.Overlay({
-    //           element: document.getElementById('popup-egypt')
-    //         });
-    //         map.addOverlay(popup_egypt);
-    //         //end marker
-    //
-    //     //Marker code for Nigeria
-    //     var pos_Nigeria = ol.proj.fromLonLat([8.6753, 9.0820]);
-    //     var markerNigeria= new ol.Overlay({
-    //          position: pos_Nigeria,
-    //          positioning: 'center-center',
-    //          element: document.getElementById('marker-Nigeria'),
-    //          stopEvent: false
-    //        });
-    //        map.addOverlay(markerNigeria);
-    //
-    //        var Nigeria = new ol.Overlay({
-    //           position: pos_Nigeria,
-    //           element: document.getElementById('nigeria')
-    //         });
-    //         map.addOverlay(Nigeria);
-    //
-    //         // Popup showing the position the user clicked
-    //         var popup_nigeria= new ol.Overlay({
-    //           element: document.getElementById('popup-nigeria')
-    //         });
-    //         map.addOverlay(popup_nigeria);
-    //         //end marker
-    //
-    //  $('.marker').on('click', function(evt) {
-    //     var title = evt.target.title;
-    //     var popup = window.map.getOverlayById(title);
-    //     //var poppy = popup.getElement();
-    //     var pop = $('#' + title);
-    //     var element = evt.target;
-    //     var coordinate = JSON.parse(evt.target.dataset.coord);
-    //     var data = evt.target.dataset.popup;
-    //     $(element).popover('dispose');
-    //     popup.setPosition(coordinate);
-    //     $(element).popover({
-    //       placement: 'top',
-    //       animation: false,
-    //       html: true,
-    //       content: data
-    //     });
-    //     $(element).popover('show');
-    //   });
+var afghanistan_marker = L.marker([33.9391,67.7100]).addTo(mymap);
+afghanistan_marker.bindPopup("In Afghanistan, men are allowed to forbid their women from the leaving the house.");
+
+var egypt_marker = L.marker([26.8206,30.8025]).addTo(mymap);
+egypt_marker.bindPopup("In Egypt, if a man kills his wife, he will receive more lenient punishment than the consequences of murder in other situations.");
+
+var nigeria_marker = L.marker([9.0820,8.6753]).addTo(mymap);
+nigeria_marker.bindPopup("In Nigeria, women can be legally assaulted by a man if that man is her husband.");
+
+var argentina_latlng = L.latLng(-36.61667,-64.28333)
+var argentina_marker = L.marker(argentina_latlng).addTo(mymap);
+argentina_marker.bindPopup("Argentina has yet to pass a law that would legalize abortion.");
